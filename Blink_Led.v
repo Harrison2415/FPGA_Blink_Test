0042925776a7b35ed8476ee2657d30d4de58dfd9
Harrison2415 <harrison1557@gmail.com>
2025-06-23 20:30:02 -0300

    Contador Led de LEDR7 ao  LEDR0
    Funcionando corretamente. A placa precisa estar em RUN para gravar.

--- Blink_Led.v
+++ Blink_Led.v
@@ -1,6 +1,9 @@
-module led_blink (
-    input CLOCK_50,          // Clock de 50 MHz
-    output [7:0] LEDR        // LEDs vermelhos
+module Project_01 (
+
+// O projeto é um blink de LED
+
+    input CLOCK_50,
+    output [7:0] LEDR
 );
 
 reg [25:0] counter = 0;
@@ -8,10 +11,9 @@ reg [7:0] led_state = 8'b00000001;
 
 always @(posedge CLOCK_50) begin
     counter <= counter + 1;
-
     if (counter >= 26'd25_000_000) begin
         counter <= 0;
-        led_state <= {led_state[6:0], led_state[7]}; // rotaciona bits
+        led_state <= {led_state[6:0], led_state[7]};
     end
 end
 
---
+++ output_files/Project_01.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 24.1std.0 Build 1077 03/04/2025 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("D:/01 Projetos Pessoal/01 Code/Projects_FPGA/FPGA_Test/output_files/") File("Project_01.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
